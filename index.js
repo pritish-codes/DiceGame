10084cfb7ffc30ca2b2365b5bb7f668eb41a9f8d
Pritish Khera <80779475+pritish-codes@users.noreply.github.com>
2022-07-21 00:14:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ if (randomNumber1>randomNumber2) {
     document.querySelector("h1").innerHTML="Player1 Wins"
     
 }
-else{
+else if(randomNumber1<randomNumber2){
     document.querySelector("h1").innerHTML="Player2 Wins"
+
+}
+else{
+     document.querySelector("h1").innerHTML="Draw"
 
 }
