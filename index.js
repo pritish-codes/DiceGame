3357e5da785f1a7019548bf49470a3eeee822872
Pritish Khera <80779475+pritish-codes@users.noreply.github.com>
2022-07-21 00:21:58 +0530

    Update Index.js(Final)

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ document.querySelector(".dice .img1").setAttribute("src",images1);
 document.querySelector(".dice .img2").setAttribute("src",images2);
 
 
+
 if (randomNumber1>randomNumber2) {
     document.querySelector("h1").innerHTML="Player1 Wins"
     
 }
-else if(randomNumber1<randomNumber2){
+if(randomNumber1<randomNumber2){
     document.querySelector("h1").innerHTML="Player2 Wins"
 
 }
-else{
-     document.querySelector("h1").innerHTML="Draw"
 
+if(randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML="Draw"
 }
